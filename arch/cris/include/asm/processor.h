ea551e70fbbdc555ead8736dcb2a92bbbb5bf5ea
1336658570 <1336658570@qq.com>
2024-08-14 23:15:12 +0800

    chap 2

--- arch/cris/include/asm/processor.h
+++ arch/cris/include/asm/processor.h
@@ -17,17 +17,25 @@
 
 struct task_struct;
 
-#define STACK_TOP	TASK_SIZE
-#define STACK_TOP_MAX	STACK_TOP
+#define STACK_TOP	TASK_SIZE	// 定义堆栈顶部为任务大小
+#define STACK_TOP_MAX	STACK_TOP	// 堆栈顶部最大值同样设为任务大小
 
 /* This decides where the kernel will search for a free chunk of vm
  * space during mmap's.
+ */
+/*
+ * 这决定了内核在进行内存映射时会在哪里搜索空闲的虚拟内存块。
  */
 #define TASK_UNMAPPED_BASE      (PAGE_ALIGN(TASK_SIZE / 3))
 
 /* THREAD_SIZE is the size of the task_struct/kernel_stack combo.
  * normally, the stack is found by doing something like p + THREAD_SIZE
  * in CRIS, a page is 8192 bytes, which seems like a sane size
+ */
+/*
+ * THREAD_SIZE 是 task_struct/kernel_stack 组合的大小。
+ * 通常，可以通过 p + THREAD_SIZE 来找到堆栈。
+ * 在 CRIS 架构中，一个页面是 8192 字节，这似乎是一个合理的大小。
  */
 
 #define THREAD_SIZE       PAGE_SIZE
@@ -40,11 +48,20 @@ struct task_struct;
  * we're in the kernel, won't affect this - only the first user->kernel transition
  * registers are reached by this.
  */
+/*
+ * 在用户到内核的转换时，pt_regs 结构被压入内核栈顶部。
+ * 这个宏允许我们为一个任务找到这些寄存器。
+ * 注意，随后的 pt_regs 压栈，比如当我们在内核中时发生的递归中断，不会影响此处 -
+ * 只有第一次用户到内核转换的寄存器可以通过此方式访问。
+ */
 
 #define user_regs(thread_info) (((struct pt_regs *)((unsigned long)(thread_info) + THREAD_SIZE)) - 1)
 
 /*
  * Dito but for the currently running task
+ */
+/*
+ * 同上，但适用于当前运行的任务
  */
 
 #define task_pt_regs(task) user_regs(task_thread_info(task))
@@ -52,12 +69,14 @@ struct task_struct;
 
 static inline void prepare_to_copy(struct task_struct *tsk)
 {
+	// 准备复制任务结构
 }
 
 extern int kernel_thread(int (*fn)(void *), void * arg, unsigned long flags);
 
 unsigned long get_wchan(struct task_struct *p);
 
+// 获取任务的用户栈指针
 #define KSTK_ESP(tsk)   ((tsk) == current ? rdusp() : (tsk)->thread.usp)
 
 extern unsigned long thread_saved_pc(struct task_struct *tsk);
@@ -66,10 +85,13 @@ extern unsigned long thread_saved_pc(struct task_struct *tsk);
 static inline void release_thread(struct task_struct *dead_task)
 {
         /* Nothing needs to be done.  */
+				// 释放线程所持有的所有资源
 }
 
+// 初始化栈
 #define init_stack      (init_thread_union.stack)
 
+// 内存屏障
 #define cpu_relax()     barrier()
 
 #endif /* __ASM_CRIS_PROCESSOR_H */
--- arch/x86/kernel/init_task.c
+++ arch/x86/kernel/init_task.c
@@ -10,7 +10,9 @@
 #include <asm/pgtable.h>
 #include <asm/desc.h>
 
+// 初始化信号结构
 static struct signal_struct init_signals = INIT_SIGNALS(init_signals);
+// 初始化信号处理结构
 static struct sighand_struct init_sighand = INIT_SIGHAND(init_sighand);
 
 /*
@@ -20,6 +22,11 @@ static struct sighand_struct init_sighand = INIT_SIGHAND(init_sighand);
  * way process stacks are handled. This is done by having a special
  * "init_task" linker map entry..
  */
+/*
+ * 初始化线程结构。
+ * 需要确保此结构按 THREAD_SIZE 对齐，因为这关系到进程堆栈的处理方式。
+ * 这是通过设置一个特别的 “init_task” 链接映射条目来实现的。
+ */
 union thread_union init_thread_union __init_task_data =
 	{ INIT_THREAD_INFO(init_task) };
 
@@ -27,6 +34,11 @@ union thread_union init_thread_union __init_task_data =
  * Initial task structure.
  *
  * All other task structs will be allocated on slabs in fork.c
+ */
+/*
+ * 初始化任务结构。
+ *
+ * 其他所有任务结构将在 fork.c 中的 slab 上分配。
  */
 struct task_struct init_task = INIT_TASK(init_task);
 EXPORT_SYMBOL(init_task);
@@ -38,5 +50,13 @@ EXPORT_SYMBOL(init_task);
  * section. Since TSS's are completely CPU-local, we want them
  * on exact cacheline boundaries, to eliminate cacheline ping-pong.
  */
+/*
+ * 每CPU的TSS段。线程在Linux上是完全“软”的，
+ * 不再有每个任务的TSS。TSS大小保持与缓存线对齐，
+ * 因此它们可以位于 .data.cacheline_aligned 部分。
+ * 由于TSS完全是CPU本地的，我们希望它们位于确切的缓存行边界上，
+ * 以避免缓存行之间的竞争。
+ */
+
 DEFINE_PER_CPU_SHARED_ALIGNED(struct tss_struct, init_tss) = INIT_TSS;
 
